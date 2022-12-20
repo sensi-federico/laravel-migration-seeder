d32b4d118168c5e44634e39c232285ec4bafd552
sensiz <dev.federicosensi@gmail.com>
2022-12-20 16:57:50 +0100

    adding seeder

--- database/migrations/2022_12_20_151627_create_train_table.php
+++ database/migrations/2022_12_20_151627_create_train_table.php
@@ -15,8 +15,28 @@ return new class extends Migration
     {
         Schema::create('train', function (Blueprint $table) {
             $table->id();
+            $table->string('azienda', 70);
+            $table->string('Stazione di partenza');
+            $table->string('Stazione di arrivo');
+            $table->string('Orario di partenza', 10);
+            $table->string('Orario di arrivo', 10);
+            $table->int('Codice treno');
+            $table->int('Numero Carrozze')->nullable();
+            $table->boolean('In orario')->default(1);
+            $table->boolean('Cancellato')->default(0);
             $table->timestamps();
         });
+
+
+        // Azienda
+        // Stazione di partenza
+        // Stazione di arrivo
+        // Orario di partenza
+        // Orario di arrivo
+        // Codice Treno
+        // Numero Carrozze
+        // In orario
+        // Cancellato
     }
 
     /**
---
+++ database/seeders/TrainTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Train;
+use Faker\Generator as Faker;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TrainTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i = 0; $i < 5; $i++) {
+            $train = new Train();
+        }
+    }
+}
